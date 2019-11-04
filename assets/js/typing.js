cd98ad7041c1409a20360ec40d52dd8f3907d780
Edwin <espined48@gmail.com>
2019-11-04 09:31:50 -0800

    Removed excess beginner test

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -3,44 +3,60 @@ var win = "for(int i = 0; i < 10; i++)";
 var wordArrayEasy = ["char my_char = '';",
                      "float my_float = 0.0;",
                      "double my_double = 0.0;",
-                     "signed char my_char = '';",
-                     "unsigned char my_char = '';",
-                     "short my_short = 0;",
-                     "short int my_sort_int = 0;",
-                     "signed short my_signed_short = 0;",
-                     "signed short int my_signed_short_int = 0;",
                      "int my_int = 0;",
-                     "signed my_signed = 0;",
                      "signed int my_signed_int = 0;",
-                     "unsigned my_unsigned = 0;",
                      "unsigned int my_unsigned_int = 0;",
-                     "long my_long = 0;",
                      "long int my_long_int = 0;",
-                     "signed long my_signed_long = 0;",
-                     "signed long int my_signed_long_int = 0;",
-                     "unsigned long my_unsigned_long = 0;",
-                     "unsigned long int my_unsigned_long_int = 0;",
-                     "long long my_long_long = 0;",
-                     "long long int my_long_long_int = 0;",
-                     "signed long long my_signed_long_long = 0;",
-                     "signed long long int my_signed_long_long_int = 0;",
-                     "unsigned long long my_unsigned_long_long = 0;",
-                     "unsigned long long int my_unsigned_long_long_int = 0;",
-                     "long double my_long_double = 0;"];
+                     "long double my_long_double = 0;",
+                     "for(int i = 0; i < 10; i++)",
+                     "while(i < 10)",
+                     ""];
 
 $("#display").html(words);
 
-$("input").keypress(function(event){
-  if(event.which === 13 && $(this).val() === words){
-    $("#display").html("Win");
-  }
-  else if(event.which === 13 && $(this).val() !== words){
-    $("#display").html("Loss");
-  }
-});
+$("textarea").keypress(function(event)
+    {
+	var TABKEY = 9;
+	var ENTERKEY = 13;
+	console.log($(this).val());
 
-$("#reset").on("click", function(){
-  $("#display").html(words);
-})
+	if(event.keyCode === TABKEY && event.preventDefault)
+	{
+	    console.log("hello");
+	    event.preventDefault();
+	    this.value += "    ";
+	}
+	
+	if($(this).val() === words && event.keyCode === ENTERKEY)
+	{
+	    $("#display").html("Win");
+	}
+	else if($(this).val() !== words && event.keyCode === ENTERKEY)
+	{
+	    $("#display").html("Loss");
+	}
+    });
+
+$("textarea").keydown(function(event)
+    {
+	var TABKEY = 9;
+
+	if(event.keyCode === TABKEY)
+	{
+	    event.preventDefault();
+	    this.value += "    ";
+	}
+    });
 
 
+$("#reset").on("click", function()
+    {
+	$("#display").html(words);
+    })
+
+
+function pickWords()
+{
+    var randomValue = Math.floor(Math.random() * wordArrayEasy);
+    
+}
